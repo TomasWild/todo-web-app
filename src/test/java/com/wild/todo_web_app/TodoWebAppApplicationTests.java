06275e8b04fca6053a4b84eb6992655f00501fe4
TomasWild <tomaswild@outlook.com>
2024-06-16 12:28:13 -0300

    initial commit

---
+++ src/test/java/com/wild/todo_web_app/TodoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wild.todo_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
